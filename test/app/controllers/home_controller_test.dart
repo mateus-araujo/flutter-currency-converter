dd407d462b9393031f413a25a428420e550b188f
Mateus Araujo <mateus_1542@hotmail.com>
2021-04-03 15:39:58 -0300

    feat(home-controller): creates HomeController with tests

---
+++ test/app/controllers/home_controller_test.dart
@@ -0,0 +1,35 @@
+import 'package:currency_converter/app/controllers/home_controller.dart';
+import 'package:currency_converter/app/models/currency_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+
+  final homeController = HomeController(toText: toText, fromText: fromText);
+
+  test('should convert from real to dolar with comma', () {
+    toText.text = '2,0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('should convert from real to dolar with dot', () {
+    toText.text = '2.0';
+    homeController.convert();
+    expect(fromText.text, '0.36');
+  });
+
+  test('should convert from dolar to real', () {
+    toText.text = '1.0';
+
+    homeController.toCurrency = CurrencyModel(
+        name: 'Dolar', real: 5.65, dolar: 1.0, euro: 0.85, bitcoin: 0.000017);
+    homeController.fromCurrency = CurrencyModel(
+        name: 'Real', real: 1.0, dolar: 0.18, euro: 0.15, bitcoin: 0.0000030);
+
+    homeController.convert();
+    expect(fromText.text, '5.65');
+  });
+}
